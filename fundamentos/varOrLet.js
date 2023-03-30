48655c5b6e31eae29b9d703553e5eb48cfc4c256
Mairy Eduardo <mairyeduardo@gmail.com>
2023-03-30 14:27:07 -0300

    teste Hoisting

--- fundamentos/varOrLet.js
+++ fundamentos/varOrLet.js
@@ -6,7 +6,7 @@ function exibirMensagem(){
        console.log(escopoBloco); // Alura 
    } 
 
-   console.log(escopoFuncao); // Caelum 
+   console.log(escopoFuncao); // Caelum , como o var possui uma variavel Hoisting ela é movida até o topo do seu contexto de execução.
    console.log(escopoBloco); // Ocorrera um erro ao printar pois Let só pode ser utilizado no bloco de criação.
 }
 
