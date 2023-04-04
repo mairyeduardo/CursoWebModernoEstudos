4b9660b2bbc359647d434c3cd239a1dc06a1a843
Mairy Eduardo <mairyeduardo@gmail.com>
2023-04-04 13:19:43 -0300

    teste finalizado

---
+++ fundamentos/string.js
@@ -0,0 +1,13 @@
+const variavel = "Cod3r"
+
+console.log(variavel.charAt(4));
+console.log(variavel.charAt(5));
+console.log(variavel.indexOf('3'));
+
+console.log(variavel.substring(1));
+console.log(variavel.substring(0,3));
+
+console.log('Palavra '.concat(variavel).concat('!'));
+console.log(variavel.replace(3, 'e'));
+
+console.log('String1,String2,String3'.split(','));
