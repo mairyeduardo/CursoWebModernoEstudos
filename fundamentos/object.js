faa6ab718c7fb823f18e398663f3c1a067d28e30
Mairy Eduardo <mairyeduardo@gmail.com>
2023-04-04 14:25:48 -0300

    teste finalizado

---
+++ fundamentos/object.js
@@ -0,0 +1,17 @@
+
+//CRIACAO DE OBJETOS
+const produto1 = {
+    nome: 'Celular Motorola',
+    preco: 2199.99
+}
+
+//or 
+const produto2 = {}
+produto2.nome = "Celular Samsung"
+produto2.preco = 3499.99
+
+//outra forma de criar atributos...
+produto2['Desconto'] = 0.40
+//
+console.log(produto1)
+console.log(produto2)
