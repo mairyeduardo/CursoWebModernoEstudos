79cc2593ff1b4d2299368ebfcec32c07bdb6687a
Mairy Eduardo <mairyeduardo@gmail.com>
2023-04-18 14:55:35 -0300

    :white_check_mark: teste finalizado

---
+++ estruturasDeControle/for1.js
@@ -0,0 +1,23 @@
+//utilizando o while para repetições determinadas: 
+
+let contador =1 
+while(contador <= 10) {
+	contador++ 
+	console.log(`contador = ${contador}`)
+}
+
+// utilizando o for: 
+
+for(let i = 1; i <= 10; i++) {
+	console.log(`i = ${i}`)
+}
+
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+
+for(let i = 0; i < notas.length; i++) {
+		console.log(`Notas = ${notas[i]}`)
+}
+
+
+
