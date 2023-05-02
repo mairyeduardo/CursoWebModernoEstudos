fa2b319d7ea8f1e3dce3b62e9b38967a9c842001
mairyeduardo <mairyeduardo@gmail.com>
2023-05-02 15:45:54 -0300

    exercicio finalizado

--- funcao/thisEBind.js
+++ funcao/thisEBind.js
@@ -7,7 +7,10 @@ const pessoa = {
 
 pessoa.falar()
 const falar = pessoa.falar
- falar() //conflito para acessar o objeto saudacao de pessoa
+ falar() //conflito para acessar o objeto saudacao de pessoa pois o this aponta para outro local
 
- const falarDePessoa = pessoa.falar.bind(pessoa)
+ const falarDePessoa = pessoa.falar.bind(pessoa) // bind passa o objeto que quer que seja resolvido o this.
  falarDePessoa()
+
+ const falar2 = pessoa.falar
+ falar2()
